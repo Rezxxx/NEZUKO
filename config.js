004482ea57c8e003b79f11d296ca154967e20363
Rezxxx <102448731+Rezxxx@users.noreply.github.com>
2022-04-07 11:34:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
+global.owner = ['6288298036183','0']
 global.packname = 'Bot Whatsapp'
-global.author = 'Nezuko'
+global.author = 'BlueDarkBot'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'BlueDarkYT'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
